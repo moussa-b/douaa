0a0e61c2525dad7509d924ff18308d445752c749
moussa <dd@>
2026-02-15 17:44:58 +0100

    Add some UI improvements

--- specs/douaa_database.sql
+++ specs/douaa_database.sql
@@ -1,50 +1,46 @@
 -- SQL Export
 -- Created by Querious (402014)
--- Created: 13 February 2026 at 23:41:28 CET
+-- Created: 15 February 2026 at 17:43:19 CET
 -- Encoding: Unicode (UTF-8)
 
+PRAGMA
+foreign_keys = OFF;
 
-PRAGMA foreign_keys = OFF;
+PRAGMA
+ignore_check_constraints = OFF;
 
-PRAGMA ignore_check_constraints = OFF;
-
-
-
-
-
-CREATE TABLE "category" (
-  "id" INTEGER NOT NULL PRIMARY KEY,
-  "name" TEXT
+CREATE TABLE "category"
+(
+    "id"         INTEGER NOT NULL DEFAULT 0,
+    "name"       TEXT             DEFAULT NULL,
+    "sort_order" INTEGER          DEFAULT NULL
 );
 
-
-CREATE TABLE "sub_category" (
-  "id" INTEGER NOT NULL PRIMARY KEY,
-  "name" TEXT
+CREATE TABLE "sub_category"
+(
+    "id"        INTEGER NOT NULL PRIMARY KEY,
+    "name"      TEXT,
+    category_id INTEGER REFERENCES category (id)
 );
 
-
-CREATE TABLE "douaa" (
-  "id" INTEGER NOT NULL PRIMARY KEY,
-  "category_id" INTEGER DEFAULT NULL,
-  "sub_category_id" INTEGER DEFAULT NULL,
-  "douaa_ar" TEXT DEFAULT NULL,
-  "douaa_fr" TEXT DEFAULT NULL,
-  "reference" TEXT DEFAULT NULL,
-  "tags" TEXT DEFAULT NULL, is_favorite INTEGER DEFAULT 0,
-  FOREIGN KEY ("category_id") REFERENCES "category" ("id"),
-  FOREIGN KEY ("sub_category_id") REFERENCES "sub_category" ("id")
+CREATE TABLE "douaa"
+(
+    "id"              INTEGER NOT NULL PRIMARY KEY,
+    "category_id"     INTEGER DEFAULT NULL,
+    "sub_category_id" INTEGER DEFAULT NULL,
+    "douaa_ar"        TEXT    DEFAULT NULL,
+    "douaa_fr"        TEXT    DEFAULT NULL,
+    "reference"       TEXT    DEFAULT NULL,
+    "tags"            TEXT    DEFAULT NULL,
+    is_favorite       INTEGER DEFAULT 0,
+    FOREIGN KEY ("category_id") REFERENCES "category" ("id"),
+    FOREIGN KEY ("sub_category_id") REFERENCES "sub_category" ("id")
 );
 
+PRAGMA
+foreign_keys = ON;
 
+PRAGMA
+ignore_check_constraints = ON;
 
-
-
-
-PRAGMA foreign_keys = ON;
-
-PRAGMA ignore_check_constraints = ON;
-
-
-
---  Export Finished: 13 February 2026 at 23:41:28 CET
+--  Export Finished: 15 February 2026 at 17:43:19 CET
